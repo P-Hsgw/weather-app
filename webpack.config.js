40284201ec0561cd206434d9b1cba457ca34f3dc
P-Hsgw <contact@p-hsgw.com>
2021-04-15 00:18:07 +0900

    Update HTML structure to make it more readable, and create IMG folder

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path');
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist')
-    },
-    devtool: 'inline-source-map',
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+      filename: 'main.js',
+      path: path.resolve(__dirname, 'dist')
+  },
+  devtool: 'inline-source-map',
 
 };
