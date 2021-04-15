fffcae184ccea97e04ca47682a845ca6e6c2a29f
P-Hsgw <contact@p-hsgw.com>
2021-04-15 14:06:43 +0900

    Add simple error handling

--- src/modules/api-calls.js
+++ src/modules/api-calls.js
@@ -1,17 +1,28 @@
-import { createCard, removeCard } from "./dom-manipulation"
+import { createCard, removeCard } from "./dom-manipulation";
 
 async function fetchWeather(search) {
-
-  const response = await fetch(
-    `http://api.weatherbit.io/v2.0/current?key=e2f4ef04485e473a929d67447201e1d2&city=${search}`,
-    { mode: "cors" }
-  );
-  const weather = await response.json();
+  try {
+    const response = await fetch(
+      `http://api.weatherbit.io/v2.0/current?key=e2f4ef04485e473a929d67447201e1d2&city=${search}`,
+      { mode: "cors" }
+    );
+    const weather = await response.json();
   
-  console.log(weather.data[0]);
-  removeCard()
-
-  createCard(weather.data[0].city_name, weather.data[0].temp, weather.data[0].rh, weather.data[0].weather.description, weather.data[0].weather.icon)
+    console.log(weather.data[0]);
+    removeCard();
+  
+    createCard(
+      weather.data[0].city_name,
+      weather.data[0].temp,
+      weather.data[0].rh,
+      weather.data[0].weather.description,
+      weather.data[0].weather.icon,
+      weather.data[0].wind_spd
+    );
+  } catch (error) {
+    alert("Error:" + error)
+  }
+  
 }
 
 function searchCity() {
--- src/modules/dom-manipulation.js
+++ src/modules/dom-manipulation.js
@@ -1,81 +1,95 @@
-const background = document.querySelector("#background")
+const background = document.querySelector("#background");
 
 function removeCard() {
   while (background.firstChild) {
-    background.firstChild.remove()
-}
-}
-
-function createCard(city, temperature, humidity, description, icon) {
-
-  const section = document.createElement("div")
-  section.classList.add("uk-section")
-
-  const div = document.createElement("div")
-
-  const cardParent = document.createElement("div")
-  cardParent.classList.add("uk-card")
-  cardParent.classList.add("uk-card-default")
-
-  const cardHeader = document.createElement("div")
-  cardHeader.classList.add("uk-card-header")
-
-  const cityHeading = document.createElement("div")
-  cityHeading.classList.add("uk-card-title")
-  cityHeading.setAttribute("id", "city")
-  cityHeading.innerHTML = city
-
-  const img = document.createElement("img")
-  img.classList.add("uk-border-circle")
-  img.setAttribute("width", "40")
-  img.setAttribute("height", "40")
-  img.setAttribute("src", `./icons/${icon}.png`)
-
-
-
-  const cardBody = document.createElement("div")
-  cardBody.classList.add("uk-card-body")
-
-  const tempParagraph = document.createElement("p")
-  tempParagraph.setAttribute("id", "temp")
-  tempParagraph.innerHTML = "Temperature: " + temperature + "°C"
-
-  const humidityParagraph = document.createElement("p")
-  humidityParagraph.setAttribute("id", "humid")
-  humidityParagraph.innerHTML = "Humidity: " + humidity + "%"
-
-  const descriptionParagraph = document.createElement("p")
-  descriptionParagraph.setAttribute("id", "description")
-  descriptionParagraph.innerHTML = description
-
-  const cardFooter = document.createElement("div")
-  cardFooter.classList.add("uk-card-footer")
-
-  const unitSwitch = document.createElement("p")
-  unitSwitch.innerHTML = "Change F to C"
-
-  background.appendChild(section)
-  section.appendChild(div)
-  div.appendChild(cardParent)
-  
-  cardParent.appendChild(cardHeader)
-  cardHeader.appendChild(img)
-
-  cardHeader.appendChild(cityHeading)
-
-
-
-  cardParent.appendChild(cardBody)
-  cardBody.appendChild(tempParagraph)
-  cardBody.appendChild(descriptionParagraph)
-  cardBody.appendChild(humidityParagraph)
-
-  cardParent.appendChild(cardFooter)
-  cardFooter.appendChild(unitSwitch)
+    background.firstChild.remove();
+  }
 }
 
+function createCard(city, temperature, humidity, description, icon, wind) {
+  const section = document.createElement("div");
+  section.classList.add("uk-section");
 
+  const div = document.createElement("div");
 
+  const cardParent = document.createElement("div");
+  cardParent.classList.add("uk-card");
+  cardParent.classList.add("uk-card-default");
 
+  const cardHeader = document.createElement("div");
+  cardHeader.classList.add("uk-card-header");
+
+  const gridHeader = document.createElement("div");
+  gridHeader.classList.add("uk-grid-small");
+  gridHeader.classList.add("uk-flex-middle");
+  gridHeader.setAttribute("uk-grid", "");
+
+  const imgDiv = document.createElement("div");
+  imgDiv.classList.add("uk-width-auto");
+
+  const img = document.createElement("img");
+  img.classList.add("uk-border-circle");
+  img.setAttribute("width", "40");
+  img.setAttribute("height", "40");
+  img.setAttribute("src", `./icons/${icon}.png`);
+
+  const gridCity = document.createElement("div");
+  gridCity.classList.add("uk-width-expand");
+
+  const cityHeading = document.createElement("h3");
+  cityHeading.classList.add("uk-card-title");
+  cityHeading.classList.add("uk-margin-remove-bottom");
+  cityHeading.setAttribute("id", "city");
+  cityHeading.innerHTML = city;
+
+  const descriptionParagraph = document.createElement("p");
+  descriptionParagraph.classList.add("uk-text-meta");
+  descriptionParagraph.classList.add("uk-margin-remove-top");
+  descriptionParagraph.setAttribute("id", "description");
+  descriptionParagraph.innerHTML = description;
+
+  const cardBody = document.createElement("div");
+  cardBody.classList.add("uk-card-body");
+
+  const tempParagraph = document.createElement("p");
+  tempParagraph.setAttribute("id", "temp");
+  tempParagraph.innerHTML = "Temperature: " + temperature + "°C";
+
+  const humidityParagraph = document.createElement("p");
+  humidityParagraph.setAttribute("id", "humid");
+  humidityParagraph.innerHTML = "Humidity: " + humidity + "%";
+
+  const windParagraph = document.createElement("p")
+  windParagraph.setAttribute("id", "wind")
+  windParagraph.innerHTML = "Wind speed: " + wind + "m/s"
+
+  const cardFooter = document.createElement("div");
+  cardFooter.classList.add("uk-card-footer");
+
+  const unitSwitch = document.createElement("p");
+  unitSwitch.innerHTML = "Change F to C";
+
+  background.appendChild(section);
+  section.appendChild(div);
+  div.appendChild(cardParent);
+
+  cardParent.appendChild(cardHeader);
+
+  cardHeader.appendChild(gridHeader);
+  gridHeader.appendChild(imgDiv);
+  imgDiv.appendChild(img);
+
+  gridHeader.appendChild(gridCity);
+  gridCity.appendChild(cityHeading);
+  gridCity.appendChild(descriptionParagraph);
+
+  cardParent.appendChild(cardBody);
+  cardBody.appendChild(tempParagraph);
+  cardBody.appendChild(windParagraph)
+  cardBody.appendChild(humidityParagraph);
+
+  cardParent.appendChild(cardFooter);
+  cardFooter.appendChild(unitSwitch);
+}
 
 export { createCard, removeCard };
