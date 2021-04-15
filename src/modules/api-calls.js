07a558ee01155c47740e673d79bc58ed05382106
P-Hsgw <contact@p-hsgw.com>
2021-04-15 11:03:20 +0900

    Add createCard and removeCard functions
    Functions are responsible for rendering weather data fetched from the API

--- src/modules/api-calls.js
+++ src/modules/api-calls.js
@@ -1,20 +1,17 @@
+import { createCard, removeCard } from "./dom-manipulation"
+
 async function fetchWeather(search) {
-  const title = document.getElementById("city");
-  const temperature = document.getElementById("temp");
-  const wind = document.getElementById("wind");
-  const humid = document.getElementById("humid");
 
   const response = await fetch(
     `http://api.weatherbit.io/v2.0/current?key=e2f4ef04485e473a929d67447201e1d2&city=${search}`,
     { mode: "cors" }
   );
-
   const weather = await response.json();
+  
   console.log(weather.data[0]);
-  title.innerHTML = weather.data[0].city_name;
-  temperature.innerHTML = `Temperature: ${weather.data[0].temp};`
-  wind.innerHTML = `Wind Speed: ${weather.data[0].wind_spd}`;
-  humid.innerHTML = `Humidity: ${weather.data[0].rh}`;
+  removeCard()
+
+  createCard(weather.data[0].city_name, weather.data[0].temp, weather.data[0].rh, weather.data[0].wind_spd, )
 }
 
 function searchCity() {
